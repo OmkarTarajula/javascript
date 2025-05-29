676836f2324d85610e4b6ef128cb228504a6ce34
OmkarSatyaLakshmi <omkarsatyalakshmitarajula@gmail.com>
2025-05-29 13:03:38 +0530

    add all the files

---
+++ classs-1.js
@@ -0,0 +1,64 @@
+var a=10;
+console.log(a);
+
+const b = 20;
+console.log(b);
+
+let c=30;
+console.log(c);
+
+// number
+var a=10;
+console.log(a);
+
+//string
+var str ="omkar";
+console.log(str);
+
+//boolean
+var bool=true;
+console.log(bool);
+
+//array
+var arr =[1,2,3,4,5];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[4]);
+
+var array =[1,2,"hello",true,false,[20,30,40],{a1:1,a2:2,a3:3}]
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[4]);
+console.log(array[5][1]);
+console.log(array[6].a2);
+
+//object
+var obj = {
+    a1:1,
+    a2:"hello",
+    a3:true,
+    a4:[1,2,3,4,5],
+    a5:{
+        b1:"hi",
+        b2:"hello",
+        b3:true,
+        b4:30,
+        b5:[10,20,30]
+    }
+}
+console.log(obj.a1);
+console.log(obj.a2);
+console.log(obj.a3);
+console.log(obj.a4[3]);
+console.log(obj.a5.b1);
+console.log(obj.a5.b3);
+console.log(obj.a5.b4);
+console.log(obj.a5.b5[2]);
+
+//  null
+var n= null;
+console.log(n);
+
+// un
+var v;
+console.log(v);
