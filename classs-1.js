4aa3ec3258e5ebe85e602647c15eb0191e183c6e
OmkarSatyaLakshmi <omkarsatyalakshmitarajula@gmail.com>
2025-06-02 08:45:54 +0530

    add the file

--- classs-1.js
+++ classs-1.js
@@ -32,7 +32,7 @@ console.log(array[4]);
 console.log(array[5][1]);
 console.log(array[6].a2);
 
-//object
+//object                 
 var obj = {
     a1:1,
     a2:"hello",
@@ -62,3 +62,104 @@ console.log(n);
 // un
 var v;
 console.log(v);
+
+
+var M={
+    name:"Meghana",
+    user1:[1,2,3,4,5,[6,7,8,9]],
+    user2:{
+        c:"Hello World",
+        d:"j",
+        t:[8,3,2,4,{
+            a1:20,
+            a2:30
+        },[2,5,7]]
+    }
+}
+console.log(M.user2.t[5][2]);
+
+
+var omkar={
+    name:"omkar",
+    obj1:{
+        user1:1,
+        user2:[1,2,3,4,5],
+        user3:{
+            a1:2,
+            a2:[1,4,6,7,{
+                b1:2,
+                b2:true,
+                b3:[1,4,6,3]
+            }]
+        }
+
+    }
+}
+
+omkar.obj1.user3.a2[4].b3[1]=7
+console.log(omkar)
+console.dir(omkar,{depth:null });
+
+
+//arithmetic operators
+var a=10;
+var x=20;
+var z=a+x;
+var d=a-x;
+var e= a/x;
+var f=a%x;
+console.log(z,d,e,f);
+
+//functions
+//function with console
+function sum(a,b,c){
+    console.log(a+b+c);
+
+}
+sum(10,20,30)
+
+
+function add(x,y,z){
+    return x+y+z;
+}
+console.log(add(10,20,30));
+
+function sumOf(a,b)
+{
+    console.log(a>b);
+}
+sumOf(10,20)
+
+
+var str="Meghana"
+console.log(str.length);
+
+var a="Bhavana"
+console.log(a.charAt(1));
+
+var s="Surya"
+console.log(s.indexOf("S"));
+
+var h="helloworld"
+console.log(h.includes("world"));
+console.log(h.includes("bye"));
+
+var str="hello"
+console.log(str.toUpperCase());
+
+var str="HELLO"
+console.log(str.toLowerCase());
+
+var a="Good system"
+console.log(a.startsWith("Go"));
+console.log(a.startsWith("sy"));
+
+var g= "apple phone"
+console.log(g.endsWith("one"));
+console.log(g.endsWith("apple"));
+
+var o = "   hello"
+console.log(o.trim());
+
+var y= "hello script"
+console.log(y.replace("hello","Java"));
