b9c861d3a5b51a7816449235d373fa70ce24d188
OmkarSatyaLakshmi <omkarsatyalakshmitarajula@gmail.com>
2025-06-02 09:36:03 +0530

    add the file

--- classs-1.js
+++ classs-1.js
@@ -130,36 +130,62 @@ function sumOf(a,b)
 }
 sumOf(10,20)
 
+// string methods 
 
+//length --> it finds the length of the string.
 var str="Meghana"
 console.log(str.length);
 
+// charAt --> we give the index number and it returns the character at that partiular index.
 var a="Bhavana"
 console.log(a.charAt(1));
 
+//indexOf ---> we give the character of the string and then it returns the indexof the letter.
 var s="Surya"
 console.log(s.indexOf("S"));
 
+// includes ---> include returns true if a particular word is present in teh string  or else it returns false.
 var h="helloworld"
 console.log(h.includes("world"));
 console.log(h.includes("bye"));
 
+// toUpperCase --> it converts all the letters in the string into upper case.
 var str="hello"
 console.log(str.toUpperCase());
 
+// toLowerCase --> it converts all the letters in the string into lower case.
 var str="HELLO"
 console.log(str.toLowerCase());
 
+// startsWith --> it will return true if we give a particular combination of characters in the string matches the starting letters of the string.
 var a="Good system"
 console.log(a.startsWith("Go"));
 console.log(a.startsWith("sy"));
 
+// endsWith --> it will return true if we give a particular combination of characters in the string matches the ending letters of the string.
 var g= "apple phone"
 console.log(g.endsWith("one"));
 console.log(g.endsWith("apple"));
 
+// trim ---> it will trims the spaces.
 var o = "   hello"
 console.log(o.trim());
 
+// replace ---> it will replace the part of the string with a new string .
 var y= "hello script"
 console.log(y.replace("hello","Java"));
+
+ // split--> it will split the string and it has three methods 
+var  k= "string is a string "
+console.log(k.split());
+console.log(k.split(""));
+console.log(k.split(" "));
+
+// reverse --> it will split the string into array and then reverse it only method 2 and 3 are possible 
+var l= "string is a string "
+console.log(k.split(" ").reverse());
+
+// join --> it changes array that we split becomes string 
+var m = "string is a string."
+console.log(m.split(" ").reverse().join(" "));
+console.log(m.split(" ").join(" "));
