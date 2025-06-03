c058496bc1d2d19487df36d99ea660769eeaa31a
OmkarSatyaLakshmi <omkarsatyalakshmitarajula@gmail.com>
2025-06-03 11:42:16 +0530

    add the file

--- practice(1).js
+++ practice(1).js
@@ -1,30 +1,3 @@
-
- var arr =[1,2,3,4,5,6,7,8,9,10]
- var a=arr[0]
- var b=arr[9]
- var c= a+b
- console.log(c)
-
- function sum(a,b,c)
- {
-    return a+b+c;
- }
- console.log(sum(10,20,30));
-
-var m="Meghana"
-var l=m.length
-
-var p="bhavana"
-var s=p.length
-
-var k=l+s
-
-console.log(k);
-
-var m="javascript"
-var n=m.indexOf("p")
-
-var k=m.indexOf("s")
-
-var q=k-n
-console.log(q);
+var str ="chandini"
+console.log(str.split(""))
+console.log(str.split("").reverse().join(""));
